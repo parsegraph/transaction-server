77b3ac633940cce48e37b47196c0bbb188b38acf
Tracy Thorn <ardiosomega@gmail.com>
2021-02-25 14:02:29 -0600

    Started Transaction page

--- budgetapp/frontend/src/components/App.js
+++ budgetapp/frontend/src/components/App.js
@@ -5,15 +5,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navbar } from "./layout/navbar/Navbar";
 import Login from ".//layout/pages/Login";
 import Accounts from "./layout/pages/Accounts";
+import Transactions from "./layout/pages/Transactions";
 import "./layout/styles/App.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Login />
       <div>
-        <Route exact path="/account" component={Accounts} />
+        <Switch>
+          <Route exact strict path="/" component={Login} />
+          <Route exact path="/account" component={Accounts} />
+          <Route exact path="/transaction" component={Transactions} />
+        </Switch>
       </div>
     </Router>
   );
---
+++ budgetapp/frontend/src/components/layout/pages/Transactions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Transactions() {
+  return (
+    <div>
+      <h1>Hello from the Transaction component!</h1>
+    </div>
+  );
+}
+
+export default Transactions;
