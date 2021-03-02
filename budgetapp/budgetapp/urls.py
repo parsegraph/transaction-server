51cede7efb311379d781fba61bc9f17150098f56
Aaron Faanes <dafrito@gmail.com>
2021-03-02 15:38:16 -0600

    Fix warnings

--- budgetapp/budgetapp/urls.py
+++ budgetapp/budgetapp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
+from frontend import views
+
 urlpatterns = [
     path('admin', admin.site.urls),
-    re_path(r'^/?$', include('frontend.urls')),
+    re_path(r'^$', views.index),
     path('', include('accounts.urls')),
     path('', include('category.urls')),
     path('', include('transaction.urls')),
