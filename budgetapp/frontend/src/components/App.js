a14671db29465122446095491253ba01c91dc8bf
Aaron Faanes <dafrito@gmail.com>
2021-03-18 18:13:04 -0500

    PrivateRoute and App

--- budgetapp/frontend/src/components/App.js
+++ budgetapp/frontend/src/components/App.js
@@ -1,67 +1,36 @@
 import React, {useEffect, useReducer, useState} from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Redirect, Switch } from "react-router-dom";
 import { Provider, useDispatch, connect } from "react-redux";
 
 import { Navbar } from "./layout/navbar/Navbar";
 import Login from ".//layout/pages/Login";
 import Accounts from "./layout/pages/Accounts";
-import Transactions from "./layout/pages/Transactions";
 import "./layout/styles/App.css";
 import SignUp from "./layout/pages/SignUp";
-import api from '../requests';
 
 import store from "../redux/store";
-import { loadLogin } from "../redux/actions/signUpActions";
 
-// function useLogin(){
-//   const emptyLogin = {user:null, token:null};
-//   const [login, setLogin] = useState(api.loadLogin() || emptyLogin);
-//   return [login, (login) => setLogin(state => {
-//     if (!login) {
-//       return {user:null, token:null};
-//     } else {
-//       return {...login};
-//     }
-//   })];
-// }
+import { createBrowserHistory } from 'history';
 
-function App({signup}) {
-  console.log(signup);
+import {PrivateRoute} from './PrivateRoute';
 
-  if (signup.user) {
-    return (
-        <Router>
-          <Navbar />
-          <main>
-            <Switch>
-              <Route exact strict path="/" component={Accounts} />
-              <Route exact path="/transaction" component={Transactions} />
-            </Switch>
-          </main>
-        </Router>
-    );
-  }
+export const history = createBrowserHistory();
 
+function App() {
   return (
-      <Router>
+      <Router history={history}>
         <Navbar />
         <main>
           <Switch>
-            <Route exact strict path="/">
-              <SignUp/>
-            </Route>
+            <PrivateRoute exact strict path="/" component={Accounts} />
             <Route exact strict path="/auth/user" component={Login} />
+            <Route exact strict path='/signup' component={SignUp} />
+            <Redirect from="*" to="/signup"/>
           </Switch>
         </main>
       </Router>
   );
 }
-
-const mapStateToProps = (state) => ({
-  signup: state.signup
-});
-
-App = connect(mapStateToProps, null)(App);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("app"));
