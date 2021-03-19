d2190fe3739b1018967f3b65cbf9602b48772b40
Tracy Thorn <ardiosomega@gmail.com>
2021-03-19 17:01:45 -0500

    use react router dom history

--- budgetapp/frontend/src/components/App.js
+++ budgetapp/frontend/src/components/App.js
@@ -1,36 +1,33 @@
 import React, {useEffect, useReducer, useState} from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch, useHistory } from "react-router-dom";
 import { Provider, useDispatch } from "react-redux";
 
 import { Navbar } from "./layout/navbar/Navbar";
-import Login from ".//layout/pages/Login";
+import Login from "./layout/pages/Login";
 import Accounts from "./layout/pages/Accounts";
 import "./layout/styles/App.css";
 import SignUp from "./layout/pages/SignUp";
 
 import store from "../redux/store";
 
-import { createBrowserHistory } from 'history';
-
 import {PrivateRoute} from './PrivateRoute';
 
-export const history = createBrowserHistory();
 
 function App() {
   return (
-      <Router history={history}>
-        <Navbar />
-        <main>
-          <Switch>
-            <PrivateRoute exact strict path="/" component={Accounts} />
-            <Route exact strict path="/auth/user" component={Login} />
-            <Route exact strict path='/signup' component={SignUp} />
-            <Redirect from="*" to="/signup"/>
-          </Switch>
-        </main>
-      </Router>
+    <>
+      <Navbar />
+      <main>
+        <Switch>
+          <PrivateRoute exact strict path="/" component={Accounts} />
+          <Route exact strict path="/auth/user" component={Login} />
+          <Route exact strict path='/signup' component={SignUp} />
+          <Redirect from="*" to="/signup"/>
+        </Switch>
+      </main>
+    </>
   );
 }
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("app"));
+ReactDOM.render(<Router><Provider store={store}><App /></Provider></Router>, document.getElementById("app"));
