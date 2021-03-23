f314ac77464834db162482339e588b63cc93b336
Aaron Faanes <dafrito@gmail.com>
2021-03-23 14:53:14 -0500

    Add SharedRoute tests

--- budgetapp/test/test.js
+++ budgetapp/test/test.js
@@ -10,6 +10,8 @@ import {Button} from '../frontend/src/components/layout/navbar/Button';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-17-updated';
 import renderer from 'react-test-renderer';
+import { SharedRoute } from '../frontend/src/components/SharedRoute';
+import { Route, MemoryRouter, Switch } from 'react-router-dom';
 
 configure({adapter: new Adapter()});
 
@@ -41,5 +43,42 @@ describe("Navbar", ()=>{
   it('should show up', ()=>{
     wrapper = createWrapper();
     expect(wrapper.containsMatchingElement(<Button className="nav-signup">Sign Up</Button>)).toEqual(true);
+  });
+});
+
+describe("SharedRoute", ()=>{
+  const createWrapper = (conf)=>{
+    const store = mockStore(conf);
+    return mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Switch>
+            <SharedRoute path='*' userComponent={()=>(<Button>user</Button>)} component={()=>(<Button>anonymous</Button>)}/>
+          </Switch>
+        </MemoryRouter>
+      </Provider>
+    );
+  };
+
+  it('renders correctly when anonymous', () => {
+    let wrapper = createWrapper({signup:{hasUserSession:false}});
+    const tree = renderer.create(wrapper).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly when logged in', () => {
+    let wrapper = createWrapper({signup:{hasUserSession:true}});
+    const tree = renderer.create(wrapper).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should show up as user if logged in', ()=>{
+    let wrapper = createWrapper({signup:{hasUserSession:true}});
+    expect(wrapper.containsMatchingElement(<Button>user</Button>)).toEqual(true)
+  });
+
+  it('should show up as anonymous if not logged in', ()=>{
+    let wrapper = createWrapper({signup:{hasUserSession:false}});
+    expect(wrapper.containsMatchingElement(<Button>anonymous</Button>)).toEqual(true)
   });
 });
